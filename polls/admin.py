698206e9c4fd73a97adfb358ff352270c4b5c79b
= <=>
2019-11-27 00:37:12 +0200

    generic views

--- polls/admin.py
+++ polls/admin.py
@@ -4,5 +4,9 @@ from .models import Choice, Question
 # Register your models here.
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
 admin.site.register(Choice)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
